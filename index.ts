672018fdf38511d957095cf31143e114f3117c59
0s1n <jpablo_166@hotmail.com>
2024-06-28 23:49:47 -0500

    feat: type basics

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-let age = 30;
+let age: number = 30;
+let firstName: string = "Mario";
+let isFictinal: boolean;
 
-console.log(age);
+age = 31;
+firstName = "Luigi";
+isFictinal = true;
+
+let planet = "Earth";
+let moons = 1;
+let isLarge = false;
+
+planet = "Saturn";
+moons = 62;
+isLarge = true;
+
+// null & undefined
+
+let something: null;
+let anotherThing: undefined;
+
+something = null;
+anotherThing = undefined;
