2fb07e6db709c1a26615136d188603aff8aedabb
0s1n <jpablo_166@hotmail.com>
2024-06-29 00:59:48 -0500

    feat: tuples

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("hello ninjas!!!");
+//--------
+// tuples
+//--------
+let person = ["Mario", 30, true];
+//-----------------
+// tuples examples
+//-----------------
+let hsla;
+hsla = [200, "100%", "50%", 1];
+let xy;
+xy = [94.7, 20.1];
+function useCoords() {
+    // get coords
+    const lat = 100;
+    const long = 50;
+    return [lat, long];
+}
+const [lat, long] = useCoords();
+//--------------
+// named tuples
+//--------------
+let user;
+user = ["Peach", 25];
+console.log(user[0]);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("hello ninjas!!!");
+//--------
+// tuples
+//--------
+
+let person: [string, number, boolean] = ["Mario", 30, true];
+
+//-----------------
+// tuples examples
+//-----------------
+
+let hsla: [number, string, string, number];
+hsla = [200, "100%", "50%", 1];
+
+let xy: [number, number];
+xy = [94.7, 20.1];
+
+function useCoords(): [number, number] {
+  // get coords
+  const lat = 100;
+  const long = 50;
+
+  return [lat, long];
+}
+
+const [lat, long] = useCoords();
+
+//--------------
+// named tuples
+//--------------
+
+let user: [name: string, age: number];
+
+user = ["Peach", 25];
+
+console.log(user[0]);
