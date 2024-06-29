fc37c4f2f52d0709d660854232392b467dcc7a35
0s1n <jpablo_166@hotmail.com>
2024-06-29 00:17:22 -0500

    feat: arrays & object literals

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-console.log("hello ninjas!!!");
+//--------
+// arrays
+//--------
+
+let names: string[] = ["Mario", "Luigi", "Peach"];
+let ages: number[] = [25, 28, 24];
+
+names.push("Bowser");
+ages.push(35);
+
+//----------------------------
+// type inference with arrays
+//----------------------------
+
+let fruits = ["apple", "pears", "bananas", "mangos"];
+
+fruits.push("peaches");
+
+const f = fruits[3];
+
+let things = [1, true, "hello"];
+
+const t = things[0];
+
+//-----------------
+// object literals
+//-----------------
+
+let user: { firstName: string; age: number; id: number } = {
+  firstName: "Mario",
+  age: 30,
+  id: 1,
+};
+
+user.firstName = "Peach";
+user.id = 2;
+
+//-------------------------------------
+// type inference with object literals
+//-------------------------------------
+
+let person = {
+  name: "Luigi",
+  score: 35,
+};
+
+person.name = "Bowser";
+person.score = 40;
+
+const score = person.score;
