be110476ba5addecfd3a7b1310ca629838fc703c
0s1n <jpablo_166@hotmail.com>
2024-06-29 01:49:53 -0500

    feat: type guards

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log("hello ninjas!!!");
+//-------------
+// type guards
+//-------------
+
+type Id = number | string;
+
+function swapIdType(id: Id) {
+  if (typeof id === "string") {
+    // can use string methods and properties
+    return parseInt(id);
+  } else {
+    // can use number methods and properties
+    return id.toString();
+  }
+}
+
+const idOne = swapIdType(1);
+const idTwo = swapIdType("2");
+
+console.log(idOne, idTwo);
+
+//-------------------
+// tagged interfaces
+//-------------------
+
+interface User {
+  type: "user";
+  username: string;
+  email: string;
+  id: Id;
+}
+
+interface Person {
+  type: "person";
+  firstName: string;
+  age: number;
+  id: Id;
+}
+
+function logDetails(value: User | Person): void {
+  if (value.type === "user") {
+    console.log(value.email, value.username);
+  }
+  if (value.type === "person") {
+    console.log(value.firstName, value.age);
+  }
+}
