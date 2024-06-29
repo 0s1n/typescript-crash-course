34eae3ae990575d6c76671b742f5445c08fb1720
0s1n <jpablo_166@hotmail.com>
2024-06-29 01:15:56 -0500

    feat: interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("hello ninjas!!!");
+//------------
+// interfaces
+//------------
+const authorOne = {
+    name: "Mario",
+    avatar: "/img/mario.png",
+};
+const newPost = {
+    title: "My first post",
+    body: "something interesting",
+    tags: ["gamming", "tech"],
+    createdAt: new Date(),
+    author: authorOne,
+};
+//---------------------------
+// as funtion argument types
+//---------------------------
+function createPost(post) {
+    console.log(`Created post: ${post.title} by ${post.author.name}`);
+}
+createPost(newPost);
+//-------------
+// with arrays
+//-------------
+let posts = [];
+posts.push(newPost);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log("hello ninjas!!!");
+//------------
+// interfaces
+//------------
+
+interface Author {
+  name: string;
+  avatar: string;
+}
+
+const authorOne: Author = {
+  name: "Mario",
+  avatar: "/img/mario.png",
+};
+
+interface Post {
+  title: string;
+  body: string;
+  tags: string[];
+  createdAt: Date;
+  author: Author;
+}
+
+const newPost: Post = {
+  title: "My first post",
+  body: "something interesting",
+  tags: ["gamming", "tech"],
+  createdAt: new Date(),
+  author: authorOne,
+};
+
+//---------------------------
+// as funtion argument types
+//---------------------------
+
+function createPost(post: Post): void {
+  console.log(`Created post: ${post.title} by ${post.author.name}`);
+}
+
+createPost(newPost);
+
+//-------------
+// with arrays
+//-------------
+
+let posts: Post[] = [];
+
+posts.push(newPost);
