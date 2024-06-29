a98e530c82c3de6c496e50e250f02bb2dd206d88
0s1n <jpablo_166@hotmail.com>
2024-06-29 00:34:20 -0500

    feat: functions

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("hello ninjas!!!");
+//----------
+// funtions
+//----------
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const subtractTwoNumbers = (a, b) => {
+    return a - b;
+};
+addTwoNumbers(3, 9);
+subtractTwoNumbers(10, 7);
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+}
+addAllNumbers([5, 7, 9, 11, 3, 2, 1]);
+//-----------------------
+// return type inference
+//-----------------------
+function formatGreeting(name, greeting) {
+    return `${greeting}, ${name}!`;
+}
+const result = formatGreeting("Mario", "Hello");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello ninjas!!!");
+//----------
+// funtions
+//----------
+
+function addTwoNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+const subtractTwoNumbers = (a: number, b: number): number => {
+  return a - b;
+};
+
+addTwoNumbers(3, 9);
+subtractTwoNumbers(10, 7);
+
+function addAllNumbers(items: number[]): void {
+  const total = items.reduce((a, c) => a + c, 0);
+  console.log(total);
+}
+
+addAllNumbers([5, 7, 9, 11, 3, 2, 1]);
+
+//-----------------------
+// return type inference
+//-----------------------
+
+function formatGreeting(name: string, greeting: string) {
+  return `${greeting}, ${name}!`;
+}
+
+const result = formatGreeting("Mario", "Hello");
